6322bc84018e2fcc90cfd09202420f62077dc90a
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-12-13 08:48:24 +0000

    created smurf component

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Smurf from './Smurf';
+
+class SmurfList extends Component {
+    render(){
+        return(
+            <div>
+                <Smurf />
+            </div>
+        );
+    }
+}
