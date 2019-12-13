6af78661899e8d4450f6e43a0016623dbb384c17
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-12-13 09:46:17 +0000

    Fleshed out the form working on onChange

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,18 +1,33 @@
 import React, {Component} from 'react';
-
 import smurfContext from '../contexts/smurfContext';
+
 class SmurfForm extends Component {
-    
+    static contextType = smurfContext;
 
     
 
     render(){
         return(
             <div>
-               <input/>
-               <input/>
-               <input/>
+                <form>
+               <input
+               name='name'
+               value={this.context.state.smurfForm.name}
+               placeholder='name'
+               
+               />
+               <input
+                name='age'
+                value={this.context.state.smurfForm.age}
+                placeholder='age'
+               />
+               <input
+                name='height'
+                value={this.context.state.smurfForm.height}
+                placeholder='height'
+               />
                <button>Add</button>
+               </form>
             </div>
         );
     }
